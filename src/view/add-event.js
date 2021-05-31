ec1f45eeefbf6086732407f78319f5bf9bf33819
Wh_Frost <whfrost63@gmail.com>
2021-05-31 12:49:13 +0400

    Подгонит названия методов под критерия

--- src/view/add-event.js
+++ src/view/add-event.js
@@ -272,7 +272,7 @@ export default class AddEvent extends SmartView {
     this._timeStartPicker = flatpickr(this.getElement().querySelector('#event-start-time-1'),
       {
         dateFormat: 'd/m/y H:i',
-        time_24hr: true,
+        'time_24hr': true,
         defaultDate: humanizeDate('DD/MM/YY HH:mm', this._data.timeStart),
         enableTime: true,
         maxDate: humanizeDate('DD/MM/YY HH:mm', this._data.timeEnd),
@@ -288,7 +288,7 @@ export default class AddEvent extends SmartView {
     this._timeEndPicker = flatpickr(this.getElement().querySelector('#event-end-time-1'),
       {
         dateFormat: 'd/m/y H:i',
-        time_24hr: true,
+        'time_24hr': true,
         defaultDate: humanizeDate('DD/MM/YY HH:mm', this._data.timeEnd),
         enableTime: true,
         minDate: humanizeDate('DD/MM/YY HH:mm', this._data.timeStart),
